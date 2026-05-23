821411f6c0e724db6a792bb25fb2bf400a58577b
杨奕峰 <foloe910430@163.com>
2026-05-23 23:46:20 +0800

    完善方法

--- src/renderer/global.d.ts
+++ src/renderer/global.d.ts
@@ -5,6 +5,7 @@ declare global {
     electron: {
       openMDFile: () => Promise<{ filePath: string; content: string } | null>
       openMDFileByPath: (filePath: string) => Promise<{ filePath: string; content: string } | null>
+      fetchUrlContent: (url: string) => Promise<{ url: string; content: string } | null>
       onTriggerOpenFile: (callback: () => void) => void
     }
   }
